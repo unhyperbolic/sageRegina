8c59c2a54fd0b23296bef25063798f8da13c8c9c
Matthias Goerner <enischte@gmail.com>
2016-09-08 00:44:12 -0700

    Moving configuration into separate file.

---
+++ extras/sageRegina/config.py
@@ -0,0 +1,11 @@
+boost_uri         = 'http://sageRegina.unhyperbolic.org/sources/boost_1_59_0.tar.bz2'
+boost_dir         = 'boost_1_59_0'
+tokyocabinet_uri  = 'http://sageRegina.unhyperbolic.org/sources/tokyocabinet-1.4.48.tar.gz'
+tokyocabinet_dir  = 'tokyocabinet-1.4.48'
+libxml_uri        = 'http://sageRegina.unhyperbolic.org/sources/libxml2-2.9.3.tar.gz'
+libxml_dir        = 'libxml2-2.9.3'
+
+# hash of commit at which regina was checked out
+regina_hash      = '4567544'
+regina_uri       = 'http://git.code.sf.net/u/matthiasgoerner/regina'
+regina_dir       = 'regina_%s' % regina_hash
--- setup.py
+++ setup.py
@@ -39,18 +39,7 @@
 
 # Get version from version.py
 exec(open('extras/sageRegina/version.py').read())
-
-boost_uri         = 'http://sageRegina.unhyperbolic.org/sources/boost_1_59_0.tar.bz2'
-boost_dir         = 'boost_1_59_0'
-tokyocabinet_uri  = 'http://sageRegina.unhyperbolic.org/sources/tokyocabinet-1.4.48.tar.gz'
-tokyocabinet_dir  = 'tokyocabinet-1.4.48'
-libxml_uri        = 'http://sageRegina.unhyperbolic.org/sources/libxml2-2.9.3.tar.gz'
-libxml_dir        = 'libxml2-2.9.3'
-
-# hash of commit at which regina was checked out
-regina_hash      = '4567544'
-regina_uri       = 'http://git.code.sf.net/u/matthiasgoerner/regina'
-regina_dir       = 'regina_%s' % regina_hash
+exec(open('extras/sageRegina/config.py').read())
 
 import glob, os, sys
 
@@ -379,6 +368,7 @@ class package_tar(SystemCommand):
          '--exclude "*~" '
          '--exclude "dist" '
          '--exclude "build" '
+         '--exclude "*.egg-info" '
          '.') % (version_name, transform_op, version_name)
         ]
 
