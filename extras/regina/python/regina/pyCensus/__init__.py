434dcb2fd986cfa0929dfd9b419231f5696271e3
Matthias Goerner <enischte@gmail.com>
2018-06-28 01:25:10 -0700

    Using regina version that can read census from pyCensus directory.
    Updating comment about TokyoCabinet.

--- extras/regina/python/regina/pyCensus/__init__.py
+++ extras/regina/python/regina/pyCensus/__init__.py
@@ -1,3 +1,14 @@
 """
 Dummy module to obtain path for census files from python.
+
+The TokyoCabinet database files .tdb are for versin version 1.0:910 and in
+little endian encoding suitable. Note that sageRegina is compiling its own
+version of TokyoCabinet so the files we supply should work on all operating
+systems and little endian platforms. 
+
+The .tdb files will be incompatible with the TokyoCabinet version shipped
+with Debian/Ubuntu because of a stupid bug: on these systems, TokyoCabinet
+is always using non-native (!) endianess (since --enable-swap was
+accidentally supplied to configure).
+
 """
