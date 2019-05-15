f60827ccdfc5e1165f899fe96d5c10df427a327c
Matthias Goerner <enischte@gmail.com>
2019-05-14 21:01:58 -0700

    Updating to regina where python/testsuite should work with python3.

--- extras/sageRegina/config.py
+++ extras/sageRegina/config.py
@@ -5,6 +5,6 @@ tokyocabinet_dir  = 'tokyocabinet-1.4.48'
 libxml_uri        = 'http://sageRegina.unhyperbolic.org/sources/libxml2-2.9.3.tar.gz'
 libxml_dir        = 'libxml2-2.9.3'
 
-regina_hash      = 'd02ff4d'
+regina_hash      = 'e4ee1fe'
 regina_uri       = 'https://github.com/regina-normal/regina.git'
 regina_dir       = 'regina_%s' % regina_hash
