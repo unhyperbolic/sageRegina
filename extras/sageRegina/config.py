227783fa7ce75c758e0c1568707fe51c60feae15
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2021-01-08 21:14:23 -0800

    Updating regina version hash and boost version.

--- extras/sageRegina/config.py
+++ extras/sageRegina/config.py
@@ -1,15 +1,16 @@
-# Boost stripped to python, iostreams and smart_ptr
+# Boost stripped to things needed by regina.
 #
 # It was created by downloading boost, running
 #    ./bootstrap.sh
-#    ./bjam tools/bcp
-#    mkdir /tmp/boost_1_68_0
-#    dist/bin/bcp python iostreams smart_ptr /tmp/boost_1_68_0
+#    ./b2 tools/bcp
+#    mkdir /tmp/boost_1_75_0
+#    dist/bin/bcp iostreams regex smart_ptr graph /tmp/boost_1_75_0
 #    cd /tmp
-#    COPYFILE_DISABLE=1 tar -cjf boost_1_68_0__python__iostreams__smart_ptr.tar.bz2 boost_1_68_0
+#    COPYFILE_DISABLE=1 tar -cjf boost_1_75_0__regina_deps.tar.bz2 boost_1_75_0
 
-boost_uri         = 'http://sageRegina.unhyperbolic.org/sources/boost_1_68_0__python__iostreams__smart_ptr.tar.bz2'
-boost_dir         = 'boost_1_68_0'
+
+boost_uri         = 'http://sageRegina.unhyperbolic.org/sources/boost_1_75_0__regina_deps.tar.bz2'
+boost_dir         = 'boost_1_75_0'
 
 tokyocabinet_uri  = 'http://sageRegina.unhyperbolic.org/sources/tokyocabinet-1.4.48.tar.gz'
 tokyocabinet_dir  = 'tokyocabinet-1.4.48'
@@ -17,6 +18,6 @@ tokyocabinet_dir  = 'tokyocabinet-1.4.48'
 libxml_uri        = 'http://sageRegina.unhyperbolic.org/sources/libxml2-2.9.3.tar.gz'
 libxml_dir        = 'libxml2-2.9.3'
 
-regina_hash      = '3a523aa'
+regina_hash      = '7152500'
 regina_uri       = 'https://github.com/regina-normal/regina.git'
 regina_dir       = 'regina_%s' % regina_hash
